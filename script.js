c9f80f3c0ed4e8b985d14596896c7dca887ccc03
Alex <eddings.sharanna@gmail.com>
2022-10-23 18:55:11 -0400

    finished Part 5, 7 and 8

--- script.js
+++ script.js
@@ -30,12 +30,72 @@ lastItem.parentElement.removeChild(lastItem);
 // *  a. Select all (.special-title) class elements and change their font-size to 2rem
 // ! Remember you might have to iterate through the list of elements
 
-document.querySelector(".special-title").forEach((specItem) => {
-  specItem.style.fontSize = "2rem";
+// document.querySelectorAll(".special-title").forEach((specItem) => {
+//   specItem.style.fontSize = "2rem";
+// });
+
+const specItem = document.querySelectorAll(".special-title"); // use querySelectorAll
+for (let i = 0; i < specItem.length; i++) {
+  specItem[i].style.fontSize = "2rem";
+  break;
+}
+
+// * === Part 5
+// *  a. Access the Past Races list and remove Chicago.
+
+// document.querySelectorAll("#past-races").forEach((text) => {
+//   if (text.textContent === "Chicago") {
+//     document.querySelector("#past-races").removeChild(text);
+//   }
+// });
+
+// const rmvChicago = function () {
+//   document.querySelector("#past-races > li").removeChild("Chicago");
+// };
+
+// * === Part 6
+// *  a. Create a new <li> element, change the new <li> text to the name of a city, and   append it to the Past Races list.
+
+const li = document.createElement("li");
+li.textContent = "Santa Monica";
+document.querySelector("#past-races").appendChild(li);
+
+// * === Part 7
+// *  a. Create a new .blog-post corresponding to the new city added in Part 6.
+// *  b. You will have to create a new <div> with class of .blog-post
+// *  c. A new <h2> with text
+// *  d. A new <p> with some text.
+// ! Think about what order you want to create the elements, and what order you want to append them in.
+// create new elements the append in order to add to Dom's Adventures -> main, heading, paragraph text
+
+const blogPost = document.createElement("div");
+blogPost.classList.add(".blog-post");
+const headingTwo = document.createElement("h2");
+headingTwo.textContent = "Santa Monica";
+const pEl = document.createElement("p");
+pEl.textContent =
+  "I Beat Johnny Tran in a rematch, he became a good guy so I let him join our family.";
+
+document.querySelector(".main").appendChild(blogPost);
+blogPost.appendChild(headingTwo);
+blogPost.appendChild(pEl);
+
+// * === Part 8
+// *  a. Load a random DOM quote on page refresh.
+// *  b. Query select the #quote-title ID element and add a click event handler.
+// ! That event handler should use the function randomQuotewhenever #quote-title is clicked.
+
+const randomQuote = function () {
+  document.querySelector("#quote-of-the-day").innerText = `"${
+    quotes[Math.floor(Math.random() * quotes.length)]
+  }"`;
+};
+
+document.querySelector("#quote-title").addEventListener("click", (event) => {
+  randomQuote();
 });
 
-// const specItem = document.querySelector(".special-title");
-// for (let i = 0; i < specItem.length; i++) {
-//   specItem[i].style.fontSize = "2rem";
-//   break;
-// }
+// * === Part 9
+// *  a. Select all .blog-post class elements.
+// *  b. Iterate through the list of .blog-post class elements
+// *  c. Apply two event handlers to each node. The first event handler should be listening for (mouseout) events while the second handler should be listening for (mouseenter) events.
